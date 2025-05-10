fe43d06b857f83d17caa47123b56986224412217
Domination <3074941664@qq.com>
2025-05-10 19:26:31 +0800

    编辑员工

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -17,4 +19,24 @@ public interface EmployeeService {
     * 新增员工
     * */
     void save(EmployeeDTO employeeDTO);
+
+    /*
+    * 分页查询员工信息
+    * */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /*
+    * 根据id查询员工
+    * */
+    void startOrStop(Integer status, Long id);
+
+    /*
+    * 根据id修改员工信息
+    * */
+    Employee getById(Long id);
+
+    /*
+    * 修改员工信息
+    * */
+    void update(EmployeeDTO employeeDTO);
 }
