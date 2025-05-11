0daa8477302a0b9e0f1bc56b1f34310d19add006
Domination <3074941664@qq.com>
2025-05-11 20:26:24 +0800

    修改菜品

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,7 +1,9 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -9,4 +11,15 @@ import java.util.List;
 public interface DishFlavorMapper {
     //批量插入口味数据
     void insertBatch(List<DishFlavor> dishFlavorList);
+
+    //根据菜品id删除口味数据
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteByDishId(Long dishId);
+
+    //批量删除
+    void deleteByDishIds(List<Long> dishIds);
+
+    //根据菜品id查询口味数据
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long dishId);
 }
